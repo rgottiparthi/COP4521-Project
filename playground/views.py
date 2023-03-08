4075dd57650dc6bb3c7fbd3eccb0789c07b27ace
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2023-03-08 17:25:24 -0500

    removed all files

--- playground/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def say_hello(request):
-    #pull data from db
-    return render(request, 'hello.html', { 'name' : 'NomNom'})
