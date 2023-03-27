2f3181b00b217f75b43dfbba692250b86c2b01e9
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2023-03-27 15:38:51 -0400

    Added Database Schema

--- NOMNOM/home/models.py
+++ NOMNOM/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Restraunt(models.Model):
+    RestrauntID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    Rating = models.FloatField(default=0)
+    NumRatings = models.IntegerField()
+    Description=models.CharField(max_length=200)
+
+class Item(models.Model):
+    RestrauntID = models.ForeignKey(Restraunt, on_delete=models.CASCADE, null=False)
+    Name = models.CharField(max_length=100)
+    Rating = models.FloatField(default=0)
+    NumRatings = models.IntegerField()
+    Description=models.CharField(max_length=200)
+    Price = models.FloatField()
+    Calories = models.IntegerField()
