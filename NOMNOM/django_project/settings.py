84f898db4e6f4aa1f19d056dec6b6fab3f09e0cc
youngcornelius <122466842+youngcornelius@users.noreply.github.com>
2023-04-12 17:46:19 -0400

    Added postgreSQL Integration

--- NOMNOM/django_project/settings.py
+++ NOMNOM/django_project/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nomnom',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
