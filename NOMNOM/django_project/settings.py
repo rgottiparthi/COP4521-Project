a3dcd2b37838511b2e2a70966913eee637a40eb7
youngcornelius <122466842+youngcornelius@users.noreply.github.com>
2023-04-14 01:46:28 -0400

    Added Login/Logout System

--- NOMNOM/django_project/settings.py
+++ NOMNOM/django_project/settings.py
@@ -135,3 +135,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGIN REDIRECT
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
